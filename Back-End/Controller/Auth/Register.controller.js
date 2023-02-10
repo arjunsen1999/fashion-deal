abb523dc9d27935bcdebd84a6e5de704ff5de78b
arjunsen <arjunsen717@gmail.com>
2023-02-10 14:50:46 +0530

    added validation into the register

--- Back-End/Controller/Auth/Register.controller.js
+++ Back-End/Controller/Auth/Register.controller.js
@@ -1,10 +1,17 @@
 // path (http://localhost:8080/auth/register)
+const { validationResult } = require('express-validator');
 
 const registerController = async (req, res) =>{
   try {
-    
+   // If any error exists then throw Error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ msg : errors.array()[0].msg });
+    }
+
+    res.send(req.body)
   } catch (error) {
-    
+    res.status(400).send({msg : "Somthing Went Wrong in Register"})
   }
 }
 
--- Back-End/Routes/Auth.route.js
+++ Back-End/Routes/Auth.route.js
@@ -1,11 +1,20 @@
 const authRouter = require('express').Router();
 const {registerController} = require("../Controller/Auth/Register.controller");
-const {loginController} = require("../Controller/Auth/Login.controller")
+const {loginController} = require("../Controller/Auth/Login.controller");
+const { body } = require('express-validator');
 
 // Register of any user, seller, admin || path (http://localhost:8080/auth/register)
-authRouter.route("/register").post(registerController);
+authRouter.route("/register").post([
+    body('fname', "Enter your first name").not().isEmpty(),
+    body('lname', "Enter your Last name").not().isEmpty(),
+    body('email', "Enter a vaild email").isEmail(),
+    body('password', "Password length must be atleast 3").isLength({ min: 3 }),
+], registerController);
 
 // Login of any user, seller, admin || path (http://localhost:8080/auth/login)
-authRouter.route("/login").post(loginController)
+authRouter.route("/login").post([
+    body('email', "Enter a vaild email").isEmail(),
+    body('password', "Password length must be atleast 3").isLength({ min: 3 }),
+], loginController);
 
 module.exports = {authRouter}
