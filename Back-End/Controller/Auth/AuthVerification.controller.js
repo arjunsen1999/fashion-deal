31afde67810ae5e0ac64a6bf5bb95be40ce2c93e
arjunsen <arjunsen717@gmail.com>
2023-02-13 15:21:44 +0530

    added token verification

---
+++ Back-End/Controller/Auth/AuthVerification.controller.js
@@ -0,0 +1,29 @@
+
+const {tokenModel} = require("../../Models/Token.model");
+const {authModel} = require("../../Models/Auth.model");
+
+const AuthVerification = async (req, res) =>{
+    try {
+        let {authId, token} = req.params;
+        let isToken = await tokenModel.findOne({
+            authId,
+            token
+        });
+        if(!isToken){
+            return res.status(404).send("<h1>404</h1>")
+        }
+
+        await authModel.updateOne({_id : authId}, {isActive : true});
+
+        await tokenModel.deleteOne({authId});
+
+        res.render("index")
+
+    } catch (error) {
+        return res.status(500).send({msg : "Somthing Went Wrong in Auth Verification", error})
+    }
+}
+
+module.exports = {
+    AuthVerification
+}
--- Back-End/index.js
+++ Back-End/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 const cors = require('cors');
+const path = require('path')
+
+let staticPath = path.join(__dirname, "/View");
+// console.log(staticPath)
 
 // Connection to the db import
 const {Connection} = require("./Config/db");
@@ -13,13 +17,16 @@ const {authRouter} = require("./Routes/Auth.route");
 // Globel Middleware
 app.use(express.json());
 app.use(cors());
+// app.use(express.static(staticPath));
+app.set('view engine', 'hbs');
 
+// app.get("/hello", (req, res) =>{
+//     res.render(`index.hbs`)
+// })
 
-// For Testing
 app.get("/", (req, res) =>{
-    res.send("<h1>Hello from other side</h1>")
-});
-
+    res.send("<h1>Hello from the other side</h1>")
+})
 
 // All Routes
     // Authentication
